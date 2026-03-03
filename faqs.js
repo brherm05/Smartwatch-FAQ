f6a44d8f4cd2f1194dc900297ef11cab0343a31e
brherm05 <braydenherm@gmail.com>
2026-03-03 14:41:41 -0600

    Added Global variables

---
+++ faqs.js
@@ -0,0 +1,4 @@
+faqImage = document.querySelector("#faq_image");
+faqImageOrigSrc = document.querySelector('#faq_image').src;
+faqImageOrigAlt = document.querySelector('#faq_image').alt;
+h2s = document.querySelectorAll('#h2');
